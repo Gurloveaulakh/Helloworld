ae5a9a016d8dfa67972d9c63415e054b2a7a5a01
gurloveaulakh <gurloveaulakh3@gmail.com>
2020-05-11 13:37:23 +1200

    commented

---
+++ Helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello gurlove!");//This is to get the out
+        }
+    }
+}
